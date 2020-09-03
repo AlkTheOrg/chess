36ec146271c787dde4f57f3074261fde700806e1
Alkan <alkanorgun@gmail.com>
2020-09-03 21:30:32 +0300

    Implemented queen class and its unit tests

---
+++ lib/chess/pieces/queen.rb
@@ -0,0 +1,14 @@
+require_relative './piece.rb'
+require 'pry'
+module Chess
+  class Queen < Piece
+    def initialize(position, color = 'white')
+      super(position, color)
+      @value = 9 # knight and bishop have the same relative value = 3
+    end
+
+    def init_unicode_value
+      @unicode_value = color == 'white' ? "\u2655" : "\u265B"
+    end
+  end
+end
