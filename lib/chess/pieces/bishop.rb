2b8772b7c3c575bd725af9561c65e614fd04779b
Alkan <alkanorgun@gmail.com>
2020-09-04 10:24:22 +0300

    Implemented Bishop class and its unit tests

---
+++ lib/chess/pieces/bishop.rb
@@ -0,0 +1,14 @@
+require_relative './piece.rb'
+require 'pry'
+module Chess
+  class Bishop < Piece
+    def initialize(position, color = 'white')
+      super(position, color)
+      @value = 3 # knight and bishop have the same relative value = 3
+    end
+
+    def init_unicode_value
+      @unicode_value = color == 'white' ? "\u2657" : "\u265D"
+    end
+  end
+end
